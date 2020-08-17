442724ad10c58441e116a83c1730129799680dad
Pyan <Pyan4444@gmail.com>
2020-08-17 11:08:39 -0400

    Initial Commit

---
+++ RobloxUsernameChecker/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace RobloxUsernameChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Input = new System.Windows.Forms.RichTextBox();
+            this.Output = new System.Windows.Forms.RichTextBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.AvailableCount = new System.Windows.Forms.Label();
+            this.TotalCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(12, 12);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(423, 184);
+            this.Input.TabIndex = 0;
+            this.Input.Text = "";
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(12, 206);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(423, 184);
+            this.Output.TabIndex = 1;
+            this.Output.Text = "";
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(12, 396);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(423, 31);
+            this.Start.TabIndex = 2;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // AvailableCount
+            // 
+            this.AvailableCount.AutoSize = true;
+            this.AvailableCount.Location = new System.Drawing.Point(143, 430);
+            this.AvailableCount.Name = "AvailableCount";
+            this.AvailableCount.Size = new System.Drawing.Size(62, 13);
+            this.AvailableCount.TabIndex = 3;
+            this.AvailableCount.Text = "Available: 0";
+            // 
+            // TotalCount
+            // 
+            this.TotalCount.AutoSize = true;
+            this.TotalCount.Location = new System.Drawing.Point(242, 430);
+            this.TotalCount.Name = "TotalCount";
+            this.TotalCount.Size = new System.Drawing.Size(43, 13);
+            this.TotalCount.TabIndex = 4;
+            this.TotalCount.Text = "Total: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 450);
+            this.Controls.Add(this.TotalCount);
+            this.Controls.Add(this.AvailableCount);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Input);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Username Checker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox Input;
+        private System.Windows.Forms.RichTextBox Output;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Label AvailableCount;
+        private System.Windows.Forms.Label TotalCount;
+    }
+}
+
---
+++ RobloxUsernameChecker/Form1.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RobloxUsernameChecker
+{
+    public partial class Form1 : Form
+    {
+        private HttpClient httpClient = new HttpClient();
+        private int total;
+        private int available;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async Task<string> CheckName(string[] names)
+        {
+            string working = "";
+            string[] strArray = names;
+            for (int index = 0; index < strArray.Length; ++index)
+            {
+                string name = strArray[index];
+                string str1 = await (await this.httpClient.GetAsync("https://api.roblox.com/users/get-by-username?username=" + name)).Content.ReadAsStringAsync();
+                try
+                {
+                    string str2 = JsonConvert.DeserializeObject<Dictionary<string, string>>(str1)["Id"];
+                }
+                catch (Exception ex)
+                {
+                    if (ex.ToString().Contains("The given key was not present in the dictionary") & name.Length > 2 & name.Length <= 20)
+                        working = working + name + "\n";
+                        ++this.available;
+                        this.AvailableCount.Text = "Available: " + this.available.ToString();
+                }
+                ++this.total;
+                this.TotalCount.Text = "Total: " + this.total.ToString();
+                name = (string)null;
+            }
+            strArray = (string[])null;
+            return working;
+        }
+
+
+        private async void Start_Click(object sender, EventArgs e)
+        {
+            this.total = 0;
+            this.available = 0;
+            this.TotalCount.Text = "Total: 0";
+            this.AvailableCount.Text = "Available: 0";
+            string[] names = this.Input.Text.Split('\n');
+            RichTextBox richTextBox = this.Output;
+            richTextBox.Text = await this.CheckName(names);
+            richTextBox = (RichTextBox)null;
+        }
+    }
+}
