04b965017a01cef9bf0bc6b09b92201cfb51794b
Pyan <Pyan4444@gmail.com>
2020-08-17 11:19:59 -0400

    Added Credits and Link To Github Page

--- RobloxUsernameChecker/Form1.Designer.cs
+++ RobloxUsernameChecker/Form1.Designer.cs
@@ -33,6 +33,8 @@
             this.Start = new System.Windows.Forms.Button();
             this.AvailableCount = new System.Windows.Forms.Label();
             this.TotalCount = new System.Windows.Forms.Label();
+            this.Link = new System.Windows.Forms.Label();
+            this.Credits = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Input
@@ -79,11 +81,31 @@
             this.TotalCount.TabIndex = 4;
             this.TotalCount.Text = "Total: 0";
             // 
+            // Link
+            // 
+            this.Link.AutoSize = true;
+            this.Link.Location = new System.Drawing.Point(91, 463);
+            this.Link.Name = "Link";
+            this.Link.Size = new System.Drawing.Size(260, 13);
+            this.Link.TabIndex = 5;
+            this.Link.Text = "https://github.com/PY44N/RobloxUsernameChecker";
+            // 
+            // Credits
+            // 
+            this.Credits.AutoSize = true;
+            this.Credits.Location = new System.Drawing.Point(117, 450);
+            this.Credits.Name = "Credits";
+            this.Credits.Size = new System.Drawing.Size(205, 13);
+            this.Credits.TabIndex = 6;
+            this.Credits.Text = "Open Source Username Checker by Pyan";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(447, 450);
+            this.ClientSize = new System.Drawing.Size(447, 485);
+            this.Controls.Add(this.Credits);
+            this.Controls.Add(this.Link);
             this.Controls.Add(this.TotalCount);
             this.Controls.Add(this.AvailableCount);
             this.Controls.Add(this.Start);
@@ -92,7 +114,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Username Checker";
+            this.Text = "Roblox Username Checker";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,6 +127,8 @@
         private System.Windows.Forms.Button Start;
         private System.Windows.Forms.Label AvailableCount;
         private System.Windows.Forms.Label TotalCount;
+        private System.Windows.Forms.Label Link;
+        private System.Windows.Forms.Label Credits;
     }
 }
 
